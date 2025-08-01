5053ddd7a2804796edbf0a77b9fdef7ee27595dc
Jason DeBacker <jdebacker@users.noreply.github.com>
2025-07-31 22:26:15 -0400

    add inital files

---
+++ run_macro_aging.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Activate Conda environment
+source activate macro-aging-env
+
+# Run simulations
+cd SimulationCode
+python simulation.py scenarios.json
+
+# process results and save to CSV
+cd ..
+mkdir -p Results
+cd SimulationCode
+python process_results.py "MacroAgingNatureSimulations" scenarios.json "../Results/macro_aging_results.csv"
