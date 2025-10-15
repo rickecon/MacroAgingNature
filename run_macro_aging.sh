c5ce0ac2f4ba522fba9fcccec3ac220179a9396e
Jason DeBacker <jdebacker@users.noreply.github.com>
2025-10-15 14:43:32 -0400

    update bash to run both main and sensitivity results

--- run_macro_aging.sh
+++ run_macro_aging.sh
@@ -5,11 +5,15 @@ source activate macro-aging-env
 
 # Run simulations
 cd SimulationCode
+# Main results
 python simulation.py scenarios.json
+# Sensitivity results
+python simulation.py hi_low_parameterizations.json
 
 # process results and save to CSV
 cd ..
 mkdir -p Results
 cd SimulationCode
 python process_results.py "simulation_results/" scenarios.json "../ResultsInPaper/macro_aging_results.csv"
+python process_results.py "simulation_results/" hi_low_parameterizations.json "../ResultsInPaper/macro_aging_sensitivity_results.csv"
 python figures.py
